d18d4b8ee9ba4e74052dff5199b8ea275e5e54f4
sparkroy <royluo@umich.edu>
2019-12-11 00:54:42 -0800

    Update MoE_role.py

--- mixture_role/MoE_role.py
+++ mixture_role/MoE_role.py
@@ -15,7 +15,6 @@ from matplotlib import pyplot as plt
 
 import torch.nn as nn
 import torch.nn.functional as F
-
 class GNNStack(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim, args, num_roles, task='node'):
         super(GNNStack, self).__init__()
@@ -27,6 +26,7 @@ class GNNStack(torch.nn.Module):
         for l in range(args.num_layers-1):
             self.convs.append(conv_model(hidden_dim, hidden_dim,num_roles))
 
+        # post-message-passing
         self.post_mp = nn.Sequential(
             nn.Linear(hidden_dim, hidden_dim), nn.Dropout(args.dropout), 
             nn.Linear(hidden_dim, output_dim))
@@ -43,10 +43,8 @@ class GNNStack(torch.nn.Module):
 
     def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
-
         if data.num_node_features == 0:
             x = torch.ones(data.num_nodes, 1)
-
         for i in range(self.num_layers):
             #print('layer ',i)
             x_roles = x[:,:self.num_roles]
@@ -57,7 +55,6 @@ class GNNStack(torch.nn.Module):
             x = torch.cat((x_roles, x_features),1)
         if self.task == 'graph':
             x = pyg_nn.global_max_pool(x, batch)
-
         x = x[:,self.num_roles:]
         x = self.post_mp(x)
 
@@ -68,24 +65,23 @@ class GNNStack(torch.nn.Module):
 
 
 class GraphSage(pyg_nn.MessagePassing):
+    """Non-minibatch version of GraphSage."""
     def __init__(self, in_channels, out_channels, num_roles, reducer='mean', 
                  normalize_embedding=True):
         super(GraphSage, self).__init__(aggr='mean')
-
         self.n_roles = num_roles
         #in_channels -= self.n_roles
         print('in_channels',in_channels)
-        self.W_g = nn.Linear()
         self.linears = nn.ModuleList()
         self.linears.append(nn.Linear(in_channels, out_channels))
         for i in range(self.n_roles-1):
             self.linears.append(nn.Linear(in_channels, out_channels))
-
+        #print('agg_lin',in_channels + out_channels)
         self.agg_list = nn.ModuleList()
         self.agg_list.append(nn.Linear(in_channels + out_channels, out_channels))
         for i in range(self.n_roles-1):
             self.agg_list.append(nn.Linear(in_channels + out_channels, out_channels))
-
+        #self.agg_lin = nn.Linear(in_channels + out_channels, out_channels)
         if normalize_embedding:
             self.normalize_emb = True
 
@@ -94,6 +90,7 @@ class GraphSage(pyg_nn.MessagePassing):
         return self.propagate(edge_index, size=(num_nodes, num_nodes), x=x)
 
     def message(self, x_i, x_j, edge_index, size):
+        #print('messaging...',x_j.shape)
         roles = x_j[:,:self.n_roles]
         #roles = x_i[:,:self.n_roles]
         x_j = x_j[:, self.n_roles:]
@@ -109,7 +106,7 @@ class GraphSage(pyg_nn.MessagePassing):
         #print(x_j.shape)
         return x_j
 
-    def update(self, aggr_out, x):        
+    def update(self, aggr_out, x):
         x_features = x[:, self.n_roles:]
         roles = x[:, :self.n_roles]
         #print('updating...',x_features.shape,aggr_out.shape)
